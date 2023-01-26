b73c16e43cf3104a87fb9055c7a63a22461511ee
MegumiMirai <2261768355@qq.com>
2023-01-26 09:54:51 +0800

    update

--- base/04_数学知识/01_质数/01_试除法判定质数.cpp
+++ base/04_数学知识/01_质数/01_试除法判定质数.cpp
@@ -1,43 +1,43 @@
-// 给定 n 个正整数 ai，判定每个数是否是质数。
-
-// 输入格式
-// 第一行包含整数 n。
-
-// 接下来 n 行，每行包含一个正整数 ai。
-
-// 输出格式
-// 共 n 行，其中第 i 行输出第 i 个正整数 ai 是否为质数，是则输出 Yes，否则输出 No。
-
-// 数据范围
-// 1≤n≤100,
-// 1≤ai≤231−1
-// 输入样例：
-// 2
-// 2
-// 6
-// 输出样例：
-// Yes
-// No
-
-// 时间复杂度 O(sqrt(n))
 #include<iostream>
+#include<cstring>
 using namespace std;
 
-bool is_prim(int x){
+const int N = 25, M = 1e8 + 10;
+int a[N];
+bool st[N], sst[M];
+int n, k, res;
+
+bool is_primes(int x){
     if(x < 2) return false;
-    for(int i = 2;  i <= x / i; i++) // 这里不要用 i <= sqrt(x) 因为每次算sqrt需要消耗时间  也不要用 i * i <= x 因为有溢出的风险
+    for(int i = 2; i <= x / i; i++)
         if(x % i == 0) return false;
     return true;
 }
 
+void dfs(int u, int sum, int now){
+
+    if(u > n){
+                
+        if(now == k && is_primes(sum)) res++;
+        
+        return;
+    }
+    
+    for(int i = 1; i <= n; i++){
+        if(!st[i]) {
+            st[i] = true;
+            dfs(i + 1, sum + a[i], now + 1);
+            st[i] = false;
+        }
+    }
+}
+
 int main(){
-    int n;
-    cin >> n;
-    while(n--){
-        int x;
-        cin >> x;
-        if(is_prim(x)) cout << "Yes" << endl;
-        else cout << "No" << endl;
-    } 
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    dfs(1, 0, 0);
+    
+    cout << res << endl;
+    
     return 0;
 }
