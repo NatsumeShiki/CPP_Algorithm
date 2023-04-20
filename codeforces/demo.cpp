b6c4e839e3bb65ab529a2c54a36c5703e90e6850
MegumiMirai <2261768355@qq.com>
2023-04-20 11:12:56 +0800

    update

--- codeforces/demo.cpp
+++ codeforces/demo.cpp
@@ -5,53 +5,50 @@
 #include<queue>
 #include<set>
 using namespace std;
- 
- set<int> s;
-typedef long long LL;
-const int N = 2e6 + 10;
-LL mod = 998244353;
-int n, q;
-int a[N];
+
+// string s;
+int n;
 
 void solve(){
-  scanf("%d", &q);
-  int first = 0, last = 0;
-  a[0] = 1;
-  while(q--){
-    int op, x;
-    scanf("%d", &op);
-    if(op == 1){
-      scanf("%d", &x);
-      a[++last] = x;
-    }else if(op == 2){
-      first++;
-    }else{
-      LL res = 0;
-      for(int i = first; i <= last; i++){
-        res = res * 10 + a[i];
-        if(res >= mod) res %= mod;
+  int t;
+  cin >> t;
+  while(t--){
+    cin >> n;
+    string str, res;
+    cin >> str;
+    // res = str;
+    // for(int i = 1; i < n; i++){
+    //   string s = str[i] + str.substr(0, i) + str.substr(i + 1);
+    //   if(res.compare(s) > 0) res = s;
+    // }
+    // cout << res <<endl;
+    int index = n + 1, i = 1;
+    char c = 'z';
+    for(; i < n; i++)
+      if(str[i] != str[i - 1])
+        break;
+    for(; i < n; i++) 
+      if(c >= str[i]){
+        c = str[i];
+        index = i;
       }
-      printf("%lld\n", res % mod);
-      if(res >= mod){
-        res = res % mod;
-        first = last + 1;
-        while(res){
-          a[--first] = res % 10;
-          res /= 10; 
-        }
-      }
-    }
+
+    if(index < n && str[index] <= str[0]) str = str[index] + str.substr(0, index) + str.substr(index + 1);
+    cout << str << endl;
   }
 }
 
 int main(){
+
+  // string s = "abc", str = "aba";
+  // cout << s.compare(str) << endl;
+
   #ifndef ONLINE_JUDGE
   freopen("in.txt", "r", stdin);
   freopen("out.txt", "w", stdout);
   // ios::sync_with_stdio(false);
   // cin.tie(0);
   #endif
-
   solve();
 
   return 0;
