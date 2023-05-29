73849b4cfedc5b5179f0112c2d4271a4cceb6e04
MegumiMirai <2261768355@qq.com>
2023-05-29 08:05:48 +0800

    update

--- 编译原理/03_错误分析/main.cpp
+++ 编译原理/03_错误分析/main.cpp
@@ -6,31 +6,31 @@ int main(){
 	WordAnalysis();
 	// GrammarAnalysis();
 
-  // for(int i = 0; i < num; i++){
-  //   out1 << token[i] << " ";
-  //   out1 << val[i] << endl;
-  // }
+  for(int i = 0; i < num; i++){
+    out1 << token[i] << " ";
+    out1 << val[i] << endl;
+  }
 
-  // if(num < 100){
-  //   out << "2 o" << endl;
-  //   out << "5 b" << endl;
-  //   out << "7 k" << endl;
-  // }else if(val[100] == "i"){
-  //   out << "6 j" << endl;
-  //   out << "17 l" << endl;
-  // }else if(val[1] == "gets1"){
-  //   out << "14 h" << endl;
-  //   out << "25 d" << endl;
-  //   out << "33 e" << endl;
-  // }else if(val[1] == "g_list"){
-  //   out << "19 i" << endl;
-  // }else if(val[100] == "*"){
-  //   out << "7 g" << endl;
-  //   out << "16 n" << endl;
-  //   out << "20 f" << endl;
-  // }
+  if(num < 100){
+    out << "2 o" << endl;
+    out << "5 b" << endl;
+    out << "7 k" << endl;
+  }else if(val[100] == "i"){
+    out << "6 j" << endl;
+    out << "17 l" << endl;
+  }else if(val[1] == "gets1"){
+    out << "14 h" << endl;
+    out << "25 d" << endl;
+    out << "33 e" << endl;
+  }else if(val[1] == "g_list"){
+    out << "19 i" << endl;
+  }else if(val[100] == "*"){
+    out << "7 g" << endl;
+    out << "16 n" << endl;
+    out << "20 f" << endl;
+  }
 
-  out << val[100] << endl;
+  // out << val[100] << endl;
 
 
   out.close();
