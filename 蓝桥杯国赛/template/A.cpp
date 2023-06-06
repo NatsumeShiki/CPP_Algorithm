c3cb6a62b5fe7af880a68a8c5b86aeced286cd76
MegumiMirai <2261768355@qq.com>
2023-06-06 09:42:43 +0800

    update

---
+++ 蓝桥杯国赛/template/A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+ 
+void solve(){
+
+}
+
+int main(){
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  // ios::sync_with_stdio(false);
+  // cin.tie(0);
+  #endif
+
+  solve();
+
+  return 0;
+}
