50187f3a3ba695d333a1ced1a2cc798218effbcd
MegumiMirai <2261768355@qq.com>
2023-06-14 20:06:06 +0800

    update

--- test/template.cpp
+++ test/template.cpp
@@ -6,75 +6,68 @@
 #include<set>
 #include<vector>
 #include<unordered_map>
-// #include<bits/stdc++.h>
 using namespace std;
-#define x first
-#define y second
-int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 
-typedef long long LL;
-typedef pair<int, int> PII;
 struct edge{
-  int a, b, c;
-  bool operator < (const edge &t) const{
-    return c < t.c;
+  double mid, len;
+  bool operator < (const edge &t)const{
+    if(mid != t.mid) return mid < t.mid;
+    return len  > t.len;
   }
 };
-
-int lowbit(int x) { return x & -x; }
-
-const int N = 10, M = 20;
-bool col[N], row[N], deg[M], rdeg[M];
-int g[N][N];
-int n;
-int res;
-
-void dfs(int x, int y, int cnt){
-  if(y == n) x++, y = 0;
-  if(x == n){
-    if(cnt == n) res++;
-    return;
-  }
-
-  dfs(x, y + 1, cnt);
-  if(g[x][y] == 1 && !row[x] && !col[y] && !deg[x + y] && !rdeg[x - y + n]){
-    row[x] = col[y] = deg[x + y] = rdeg[x - y + n] = true;
-    dfs(x, y + 1, cnt + 1);
-    row[x] = col[y] = deg[x + y] = rdeg[x - y + n] = false;
-  }
-}
-
-int s[13] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int N = 1e5 + 10;
+edge edges[N];
+int n, m;      
 
 void solve(){
-  int n = 100;
-  int arr[100];
-  for(int i = 0; i < n; i++) cin >> arr[i];
-  set<int> S;
-  int cnt = 0;
-  for(int a = 0; a < n; a++)
-    for(int b = a + 1; b < n; b++)
-      for(int c = b + 1; c < n; c++)
-        for(int d = c + 1; d < n; d++){
-          int year = arr[a] * 1000 + arr[b] * 100 + arr[c] * 10 + arr[d];
-          if(year != 2023) continue;
-          for(int e = d + 1; e < n; e++)
-            for(int f = e + 1; f < n; f++){
-              int month = arr[e] * 10 + arr[f];
-              if(month < 1 || month > 12) continue;
-              for(int g = f + 1; g < n; g++)
-                for(int h = g + 1; h < n; h++){
-                  int day = arr[g] * 10 + arr[h];
-                  if(day >= 1 && day <= s[month]){
-                    S.insert(year * 1000 + month * 100 + day);
-                    cnt++;
-                  }
-                } 
-            }
-        }
+  cin >> n >> m;
+  for(int i = 0; i < n; i++){
+    double l, r;
+    cin >> l >> r;
+    edges[i] = {(l + r) / 2, (r - l) / 2};
+  }
 
-  cout << S.size() << endl;
-  cout << cnt << endl;
+  sort(edges, edges + n);
+
+  while(m--){
+    double start, end;
+    cin >> start >> end;
+
+    int l = 0, r = n - 1;
+    while(l < r){
+      int mid = l + r >> 1;
+      if(edges[mid].mid < start) l = mid + 1;
+      else r = mid;
+    }
+
+    int ll = l, rr;
+    r = n - 1;
+    while(l < r){
+      int mid = l + r + 1 >> 1;
+      if(edges[mid].mid <= end) l = mid ;
+      else r = mid - 1;
+    }
+
+    rr = l;
+
+    l = ll, r = rr;
+    while(l < r){
+      int mid = l + r >> 1;
+      if(edges[mid].len > end - start) l = mid + 1;
+      else r = mid;
+    }
+
+    ll = l, r = rr;
+    while(l < r){
+      int mid = l + r + 1 >> 1;
+      if(edges[mid].len <= end - start) l = mid;
+      else r = mid - 1;
+    }
+
+    rr = l;
+    cout << rr - ll + 1 << endl;
+  }
+
 }
 
 signed main(){
