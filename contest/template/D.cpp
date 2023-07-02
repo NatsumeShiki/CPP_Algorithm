0f33d8684624a23a3b91e2f92b4d51ae85507978
MegumiMirai <2261768355@qq.com>
2023-07-02 09:57:53 +0800

    update

--- contest/template/D.cpp
+++ contest/template/D.cpp
@@ -1,18 +1,15 @@
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<vector>
-#include<queue>
-#include<set>
-#include<vector>
-// #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
 #define x first
 #define y second
 int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 
 typedef long long LL;
 typedef pair<int, int> PII;
+typedef pair<int, PII> PIII;
+typedef pair<int, PIII> PIIII;
 struct edge{
   int a, b, c;
   bool operator < (const edge &t) const{
@@ -24,13 +21,10 @@ int lowbit(int x) { return x & -x; }
 
 const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f;
 int n, m, k, t;
+vector<int> a(N);
 
 void solve(){
-  int T;
-  cin >> T;
-  while(T--){
 
-  }
 }
 
 signed main(){
@@ -41,7 +35,11 @@ signed main(){
   // cin.tie(0);
   #endif
 
-  solve();
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
 
   return 0;
 }
--- contest/template/template.cpp
+++ contest/template/template.cpp
@@ -1,18 +1,15 @@
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<vector>
-#include<queue>
-#include<set>
-#include<vector>
-// #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
+
+#define int long long
 #define x first
 #define y second
 int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
 
 typedef long long LL;
 typedef pair<int, int> PII;
+typedef pair<int, PII> PIII;
+typedef pair<int, PIII> PIIII;
 struct edge{
   int a, b, c;
   bool operator < (const edge &t) const{
@@ -24,13 +21,10 @@ int lowbit(int x) { return x & -x; }
 
 const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f;
 int n, m, k, t;
+vector<int> a(N);
 
 void solve(){
-  int T;
-  cin >> T;
-  while(T--){
 
-  }
 }
 
 signed main(){
@@ -38,105 +32,14 @@ signed main(){
   freopen("in.txt", "r", stdin);
   freopen("out.txt", "w", stdout);
   // ios::sync_with_stdio(false);
-  // cin.tie(0);F
+  // cin.tie(0);
   #endif
 
-  solve();
-
-  return 0;
-}
-
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<vector>
-#include<queue>
-#include<set>
-#include<vector>
-// #include<bits/stdc++.h>
-using namespace std;
-#define x first
-#define y second
-int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
-
-typedef long long LL;
-typedef pair<int, int> PII;
-struct edge{
-  int a, b, c;
-  bool operator < (const edge &t) const{
-    return c < t.c;
-  }
-};
-
-int lowbit(int x) { return x & -x; }
-
-const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f;
-int n, m, k, t;
-
-void solve(){
-  int T;
+  int T = 1;
   cin >> T;
   while(T--){
-
+    solve();
   }
-}
-
-signed main(){
-  #ifndef ONLINE_JUDGE
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-  // ios::sync_with_stdio(false);
-  // cin.tie(0);F
-  #endif
-
-  solve();
-
-  return 0;
-}
-#include<iostream>
-#include<cstring>
-#include<algorithm>
-#include<vector>
-#include<queue>
-#include<set>
-#include<vector>
-// #include<bits/stdc++.h>
-using namespace std;
-#define x first
-#define y second
-int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
-
-typedef long long LL;
-typedef pair<int, int> PII;
-struct edge{
-  int a, b, c;
-  bool operator < (const edge &t) const{
-    return c < t.c;
-  }
-};
-
-int lowbit(int x) { return x & -x; }
-
-const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f;
-int n, m, k, t;
-
-void solve(){
-  int T;
-  cin >> T;
-  while(T--){
-    
-  }
-}
-
-signed main(){
-  #ifndef ONLINE_JUDGE
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-  // ios::sync_with_stdio(false);
-  // cin.tie(0);F
-  #endif
-
-  solve();
 
   return 0;
 }
