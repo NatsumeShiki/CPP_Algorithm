dad33d505d69cf0681ece5ecb4166aa6caf1c8b1
MegumiMirai <2261768355@qq.com>
2023-07-03 09:44:17 +0800

    update

--- contest/template/C.cpp
+++ contest/template/C.cpp
@@ -1,7 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define int long long
 #define x first
 #define y second
 int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
@@ -21,7 +20,6 @@ int lowbit(int x) { return x & -x; }
 
 const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f;
 int n, m, k, t;
-vector<int> a(N);
 
 void solve(){
 
