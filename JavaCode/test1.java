394d74f7d5da32f22f3347966321c424c2aa4b92
MegumiMirai <2261768355@qq.com>
2023-09-28 10:41:52 +0800

    update

---
+++ JavaCode/test1.java
@@ -0,0 +1,9 @@
+package JavaCode;
+
+public class test1 {   
+   public static void main(String args[]) {          
+      String palindrome = "Dot saw I was Tod";                    
+      int len = palindrome.length();               
+      System.out.println( "String Length is : " + len );             
+   }           
+}  
