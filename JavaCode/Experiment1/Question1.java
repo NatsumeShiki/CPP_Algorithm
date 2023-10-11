2295b83c961eaa3e5f5a600570a26ea0a504e60c
MegumiMirai <2261768355@qq.com>
2023-10-11 09:20:32 +0800

    update

--- JavaCode/Experiment1/Question1.java
+++ JavaCode/Experiment1/Question1.java
@@ -6,7 +6,9 @@ import java.util.*;
 public class Question1{
   static private final String INPUT = "D:/CPP/Algorithm/JavaCode/in.txt";
   static private final String OUTPUT = "D:/CPP/Algorithm/JavaCode/out.txt";
-
+  static int n = 9;
+  static int[] arr = new int[n];
+  static int[] x = new int[1], y = new int[1];
   public static void main(String[] args) {
     
     FileInputStream fis = null;
@@ -21,8 +23,67 @@ public class Question1{
     }
     Scanner sc = new Scanner(System.in);
 
-
+    int T = sc.nextInt();
+    while(T-- > 0){
+      for(int i = 0; i < n; i++){
+        arr[i] = sc.nextInt();
+      }
+      int res = dfs1();
+      if(res == -1) System.out.println("无解");
+      else System.out.println(res);
+      res = dfs2();
+      if(res == -1) System.out.println("无解");
+      else System.out.println(res);
+    }
 
     sc.close();
+  }
+
+  // 蛮力法
+  public static int dfs1(){
+    for(int i = 0; ; i++){
+      boolean flag = true;
+      for(int j = 0, k = 10; j < n; j++, k--){
+        if(i % k != arr[j]){
+          flag = false;
+          break;
+        }
+      }
+      if(flag) return i;
+    }
+  }
+
+
+  public static int dfs2(){
+    x[0] = y[0] = 0;
+    boolean flag = true;
+    int a1 = 10, b1 = arr[0];
+    for(int i = 1; i < n; i++){
+      int a2 = 10 - i, b2 = arr[i];
+      int d = exgcd(a1, a2, x, y);
+      if((b2 - b1) % d != 0){
+        flag = false;
+        break;
+      }
+
+      x[0] *= (b2 - b1) / d;
+      int t = a2 / d;
+      x[0] = (x[0] % t + t) % t;
+      b1 = a1 * x[0] + b1;
+      a1 = Math.abs(a1 / d * a2);
+    }
+    if(flag) return (b1 % a1 + a1) % a1;
+    return -1;
+  }
+
+  public static int exgcd(int a, int b, int[] x, int[] y){
+    if(b == 0){
+      x[0] = 1;
+      y[0] = 0;
+      return a;
+    }
+    int d = exgcd(b, a % b, y, x);
+    y[0] -= a / b * x[0];
+    return d;
   }
 }
--- JavaCode/Experiment1/Question2.java
+++ JavaCode/Experiment1/Question2.java
@@ -7,6 +7,10 @@ public class Question2{
   static private final String INPUT = "D:/CPP/Algorithm/JavaCode/in.txt";
   static private final String OUTPUT = "D:/CPP/Algorithm/JavaCode/out.txt";
 
+  static int n;
+  static int m;
+  static int[][] arr;
+
   public static void main(String[] args) {
     
     FileInputStream fis = null;
@@ -21,8 +25,49 @@ public class Question2{
     }
     Scanner sc = new Scanner(System.in);
 
-
+    n = sc.nextInt();
+    m = sc.nextInt();
+    arr = new int[n][m];
+    dfs(0, 0, 1, -1, 1);
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < m; j++)
+        System.out.printf("%2d ", arr[i][j]);
+      System.out.println();
+    }
 
     sc.close();
+  }
+  
+  static void dfs(int x, int y, int cnt, int dx, int dy){
+    arr[x][y] = cnt;
+    if(x == n - 1 &&  y == m - 1) return;
+    
+    int a = x + dx;
+    int b = y + dy;
+    // System.out.print(x + " " +  y + " " + a + " " + b + " ");
+    // System.out.println(x + " " + y);
+    
+    if(a < 0 && b < m){
+      dx = 1;
+      dy = -1;
+      y++;
+    }else if(a >= n){
+      dx = -1;
+      dy = 1;
+      y++;
+    }else if(b < 0){
+      dx = -1;
+      dy = 1;
+      x++;
+    }else if(b >= m){
+      dx = 1;
+      dy = -1;
+      x++;
+    }else{
+      x += dx;
+      y += dy;
+    }
+    // System.out.println(x + " " + y);
+    dfs(x, y, cnt + 1, dx, dy);
   }
 }
