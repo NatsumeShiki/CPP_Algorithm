eec7ac2d88ae190a02d3d7989c0f35c5e2cb99ce
MegumiMirai <2261768355@qq.com>
2023-10-26 10:23:46 +0800

    update

---
+++ JavaCode/template/Question2.java
@@ -0,0 +1,28 @@
+package JavaCode.template;
+
+import java.io.*;
+import java.util.*;
+
+public class Question2{
+  static private final String INPUT = "D:/CPP/Algorithm/JavaCode/in.txt";
+  static private final String OUTPUT = "D:/CPP/Algorithm/JavaCode/out.txt";
+
+  public static void main(String[] args) {
+    
+    FileInputStream fis = null;
+    PrintStream ps = null;
+    try {
+      fis = new FileInputStream(INPUT);
+      ps = new PrintStream(new FileOutputStream(OUTPUT));
+      System.setIn(fis);
+      System.setOut(ps);
+    } catch (Exception e) {
+      System.out.println("Error Occurred.");
+    }
+    Scanner sc = new Scanner(System.in);
+
+
+
+    sc.close();
+  }
+}
