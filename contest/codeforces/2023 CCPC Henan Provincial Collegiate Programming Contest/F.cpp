e9223bac26c86943f99e881961d53dede22a2991
MegumiMirai <2261768355@qq.com>
2023-11-18 12:09:08 +0800

    update

---
+++ contest/codeforces/2023 CCPC Henan Provincial Collegiate Programming Contest/F.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define x first
+#define y second
+#define endl "\n"
+#define rep(i,a,b) for(int i = int(a);i <= int(b);++i)
+#define rer(i,a,b) for(int i = int(b);i >= int(a);--i)
+#define pep(i,a,b) for(int i = int(a);i < int(b);++i)
+#define per(i,a,b) for(int i = int(b);i > int(b);--i);
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<PII, PII> PIIII;
+typedef pair<string, int> PSI;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<char, int> PCI;
+struct edge{
+  int a, b, c;
+  bool operator < (const edge &t) const{
+    return c < t.c;
+  }
+};
+
+int lowbit(int x) { return x & -x; }
+
+const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f, mod = 998244353;
+int n, m, k, t, q;
+int arr[M];
+vector<int> alls;
+
+void solve(){
+  cin >> n >> m;
+  for(int i = 0; i < n; i++) cin >> arr[i];
+  sort(arr, arr + n);
+
+  priority_queue<PII, vector<PII>, greater<PII>> heap;
+  int res = 1e18;
+  for(int i = 0; i < n; i++){
+    if(i) heap.push({arr[i] - arr[i - 1], i - 1});
+    while(heap.size() && heap.top().y <= i - m) heap.pop();
+    if(i >= m - 1) res = min(res, heap.top().x * (arr[i] - arr[i - m + 1]));
+  }
+  cout << res << endl;
+}
+
+signed main(){
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int T = 1;
+  // cin >> T;
+  while(T--){
+    solve();
+  }
+
+  return 0;
+}
