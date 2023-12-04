7c388fe22f6f10791ebe2aa12c263a37bcc4dcc4
MegumiMirai <2261768355@qq.com>
2023-12-04 13:01:53 +0800

    update

---
+++ contest/codeforces/Codeforces Round 855 (Div. 3)/A.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define x first
+#define y second
+#define endl "\n"
+#define all(a) a.begin(), a.end()
+#define rep(i,a,b) for(int i = int(a);i <= int(b);++i)
+#define rer(i,a,b) for(int i = int(b);i >= int(a);--i)
+#define pep(i,a,b) for(int i = int(a);i < int(b);++i)
+#define per(i,a,b) for(int i = int(b);i > int(b);--i);
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<PII, PII> PIIII;
+typedef pair<string, int> PSI;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<char, int> PCI;
+struct edge{
+  int a, b, c;
+  bool operator < (const edge &t) const{
+    return c < t.c;
+  }
+};
+
+int lowbit(int x) { return x & -x; }
+
+const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f3f3f3f3f, mod = 998244353;
+int n, m, k, t, q;
+int arr[M];
+string s, str;
+char ch1[4] = {'m', 'e', 'o', 'w'};
+char ch2[4] = {'M', 'E', 'O', 'W'};
+bool st[4];
+
+void solve(){
+  cin >> n >> s;
+  int index = 0;
+  for(int i = 0; i < 4; i++){
+    st[i] = false;
+    while(index < n && (s[index] == ch1[i] || s[index] == ch2[i])) {
+      index++;
+      st[i] = true;
+    }
+    if(!st[i]){
+      cout << "No\n";
+      return;
+    }
+  }
+  // cout << index << " ";
+  if(index == n) cout << "Yes\n";
+  else cout << "No\n";
+}
+
+signed main(){
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+
+  return 0;
+}
