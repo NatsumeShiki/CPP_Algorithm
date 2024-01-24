a5f3f95cba29f4e4ea194a91dee2b6dd638eee89
MegumiMirai <2261768355@qq.com>
2024-01-24 10:14:03 +0800

    update

---
+++ WIT/WIT集训队寒假第一次训练/G.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define x first
+#define y second
+#define endl "\n"
+#define all(a) a.begin(), a.end()
+#define rep(i,a,b) for(int i = int(a);i <= int(b);++i)
+#define rer(i,a,b) for(int i = int(b);i >= int(a);--i)
+#define pep(i,a,b) for(int i = int(a);i < int(b);++i)
+#define per(i,a,b) for(int i = int(b);i > int(b);--i);
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<PII, PII> PIIII;
+typedef pair<string, int> PSI;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<char, int> PCI;
+struct edge{
+  int a, b, c;
+  bool operator < (const edge &t) const{
+    return c < t.c;
+  }
+};
+
+int lowbit(int x) { return x & -x; }
+
+const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f3f3f3f3f, mod = 998244353;
+int n, m, k, t, q;
+int arr[M];
+string s, str;
+vector<int> res;
+
+void solve(){
+  cin >> n;
+  int res = INF;
+  for(int i = 0; i <= 1e6; i++){
+    int l = 0, r = 1e6;
+    while(l < r){
+      int mid = l + r >> 1;
+      int a = i * i + mid * mid;
+      int b = i + mid;
+      if(a * b >= n) r = mid;
+      else l = mid + 1;
+    }
+    res = min(res, (i * i + l * l) * (i + l));
+  }
+  cout << res << endl;
+}
+
+signed main(){
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int T = 1;
+  while(T--){
+    solve();
+  }
+
+  return 0;
+}
