b69ca6e037a942ad76d7975d1d2657a76c7673aa
MegumiMirai <2261768355@qq.com>
2024-02-03 10:27:12 +0800

    update

---
+++ WIT/WIT集训队寒假第三次训练/E.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define x first
+#define y second
+#define endl "\n"
+#define all(a) a.begin(), a.end()
+#define rep(i,a,b) for(int i = int(a);i <= int(b);++i)
+#define rer(i,a,b) for(int i = int(b);i >= int(a);--i)
+#define pep(i,a,b) for(int i = int(a);i < int(b);++i)
+#define per(i,a,b) for(int i = int(b);i > int(b);--i);
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<PII, PII> PIIII;
+typedef pair<string, int> PSI;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<char, int> PCI;
+struct edge{
+  int a, b, c;
+  bool operator < (const edge &t) const{
+    return c < t.c;
+  }
+};
+
+int lowbit(int x) { return x & -x; }
+
+const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f3f3f3f3f, mod = 998244353;
+int n, m, k, t, q;
+int arr1[M], arr2[M];
+string s, str;
+
+void solve(){
+  cin >> n;
+  int a = 0, b = 0;
+  for(int i = 0; i < n * 2; i++){
+    int x, y;
+    cin >> x >> y;
+    x = abs(x);
+    y = abs(y);
+    if(x == 0) arr1[a++] = y;
+    else arr2[b++] = x; 
+  }
+  sort(arr1, arr1 + n);
+  sort(arr2, arr2 + n);
+  double res = 0;
+  for(int i = 0; i < n; i++){
+    int a = arr1[i], b = arr2[i];
+    res += sqrt(a * a + b * b);
+  }
+  printf("%.10lf\n", res);
+}
+
+signed main(){
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+
+  return 0;
+}
