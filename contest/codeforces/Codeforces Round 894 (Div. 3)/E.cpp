0d42efecdab0d2108e9c59214205653aef486a51
MegumiMirai <2261768355@qq.com>
2024-02-20 13:37:49 +0800

    update

---
+++ contest/codeforces/Codeforces Round 894 (Div. 3)/E.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define x first
+#define y second
+#define endl "\n"
+#define all(a) a.begin(), a.end()
+#define rep(i,a,b) for(int i = int(a);i <= int(b);++i)
+#define rer(i,a,b) for(int i = int(b);i >= int(a);--i)
+#define pep(i,a,b) for(int i = int(a);i < int(b);++i)
+#define per(i,a,b) for(int i = int(b);i > int(b);--i);
+int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<PII, int> PIII;
+typedef pair<PII, PII> PIIII;
+typedef pair<string, int> PSI;
+typedef pair<int, string> PIS;
+typedef pair<int, char> PIC;
+typedef pair<char, int> PCI;
+struct edge{
+  int a, b, c;
+  bool operator < (const edge &t) const{
+    return c < t.c;
+  }
+};
+
+int lowbit(int x) { return x & -x; }
+
+const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f3f3f3f3f, mod = 998244353;
+int n, m, k, t, q;
+int arr[M];
+string s, str;
+
+void solve(){
+  cin >> n >> m >> k;
+  for(int i = 1; i <= n; i++) cin >> arr[i];
+
+  int res = 0;
+  priority_queue<int, vector<int>, greater<int>> heap;
+
+  int sum = 0;
+  for(int i = 1; i <= n; i++){
+    if(arr[i] < 0) continue;
+    if(heap.size() < m){
+      heap.push(arr[i]);
+      sum += arr[i];
+    }else if(heap.top() < arr[i]){
+      sum -= heap.top();
+      heap.pop();
+      heap.push(arr[i]);
+      sum += arr[i];
+    }
+    res = max(res, sum - k * i);
+    // cout << heap.size() << " " << sum << " " << res << endl;
+  }
+  cout << res << endl;
+  // cout << endl;
+}
+
+signed main(){
+  #ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  #endif
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int T = 1;
+  cin >> T;
+  while(T--){
+    solve();
+  }
+
+  return 0;
+}
